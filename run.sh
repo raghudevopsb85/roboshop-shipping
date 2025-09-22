57a5d4d3cb81120b47c50be30a1f029da4d8ad5f
Raghu K <raghuk.vit@gmail.com>
2025-09-22 08:46:13 +0530

    RoboShop

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-java -XX:MaxRAMPercentage=90 -jar /app/shipping.jar
+java -javaagent:/usr/local/newrelic/newrelic.jar -Dnewrelic.config.license_key=${NEW_RELIC_LICENCE_KEY} -Dnewrelic.config.license_key=shipping -XX:MaxRAMPercentage=90 -jar /app/shipping.jar
+
+
